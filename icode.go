aa2482235a947c460e0f7ac94e0ee78bf7d3b3a0
jun <leebduk@gmail.com>
2018-08-09 17:46:04 +0900

    remove handler pkg

--- icode.go
+++ icode.go
@@ -21,14 +21,17 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
 
+	"encoding/json"
+
 	"github.com/it-chain/sdk"
 	"github.com/it-chain/sdk/logger"
+	"github.com/it-chain/sdk/pb"
 	"github.com/jessevdk/go-flags"
-	"github.com/junbeomlee/learn-icode/handler"
 )
 
 var opts struct {
@@ -49,12 +52,142 @@ func main() {
 
 	fmt.Println("port : " + strconv.Itoa(opts.Port))
 
-	exHandler := &handler.HandlerExample{}
+	exHandler := &HandlerExample{}
 	ibox := sdk.NewIBox(opts.Port)
 	ibox.SetHandler(exHandler)
 	err = ibox.On(30)
 
 	if err != nil {
 		panic(err.Error())
+	}
+}
+
+type HandlerExample struct {
+}
+
+func (*HandlerExample) Name() string {
+	return "sample"
+}
+
+func (*HandlerExample) Versions() []string {
+	vers := make([]string, 0)
+	vers = append(vers, "1.0")
+	vers = append(vers, "1.2")
+	return vers
+}
+
+func (*HandlerExample) Handle(request *pb.Request, cell *sdk.Cell) *pb.Response {
+	switch request.Type {
+	case "invoke":
+		return handleInvoke(request, cell)
+	case "query":
+		return handleQuery(request, cell)
+	case "test":
+		fmt.Println("req : " + request.Uuid)
+		if request.Uuid == "0" {
+			cell.PutData("test", []byte("0"))
+			return responseSuccess(request, []byte(string(0)))
+		}
+		data, err := cell.GetData("test")
+		if err != nil {
+			return responseError(request, err)
+		}
+		if len(data) == 0 {
+			err := errors.New("no data err")
+			return responseError(request, err)
+		}
+		strData := string(data)
+		intData, err := strconv.Atoi(strData)
+		if err != nil {
+			return responseError(request, err)
+		}
+		intData = intData + 1
+		changeData := strconv.Itoa(intData)
+		err = cell.PutData("test", []byte(changeData))
+		if err != nil {
+			return responseError(request, err)
+		}
+		return responseSuccess(request, []byte(changeData))
+	default:
+		logger.Debug(nil, "unknown request type")
+		err := errors.New("unknown request type")
+		return responseError(request, err)
+	}
+}
+func handleQuery(request *pb.Request, cell *sdk.Cell) *pb.Response {
+	switch request.FunctionName {
+	case "getA":
+		b, err := cell.GetData("A")
+
+		if err != nil {
+			return responseError(request, err)
+		}
+
+		result := make(map[string]string)
+		result["A"] = string(b)
+
+		d, err := json.Marshal(result)
+
+		if err != nil {
+			return responseError(request, err)
+		}
+
+		return responseSuccess(request, d)
+
+	default:
+		err := errors.New("unknown query method")
+		return responseError(request, err)
+	}
+}
+func handleInvoke(request *pb.Request, cell *sdk.Cell) *pb.Response {
+	switch request.FunctionName {
+	case "initA":
+		err := cell.PutData("A", []byte("0"))
+		if err != nil {
+			return responseError(request, err)
+		}
+		return responseSuccess(request, nil)
+	case "incA":
+		data, err := cell.GetData("A")
+		if err != nil {
+			return responseError(request, err)
+		}
+		if len(data) == 0 {
+			err := errors.New("no data err")
+			return responseError(request, err)
+		}
+		strData := string(data)
+		intData, err := strconv.Atoi(strData)
+		if err != nil {
+			return responseError(request, err)
+		}
+		intData++
+		changeData := strconv.Itoa(intData)
+		err = cell.PutData("A", []byte(changeData))
+		if err != nil {
+			return responseError(request, err)
+		}
+		return responseSuccess(request, nil)
+	default:
+		err := errors.New("unknown invoke method")
+		return responseError(request, err)
+	}
+}
+
+func responseError(request *pb.Request, err error) *pb.Response {
+	return &pb.Response{
+		Uuid:  request.Uuid,
+		Type:  request.Type,
+		Data:  nil,
+		Error: err.Error(),
+	}
+}
+
+func responseSuccess(request *pb.Request, data []byte) *pb.Response {
+	return &pb.Response{
+		Uuid:  request.Uuid,
+		Type:  request.Type,
+		Data:  data,
+		Error: "",
 	}
 }
